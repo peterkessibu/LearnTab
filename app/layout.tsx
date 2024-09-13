93be13d470b9388ce5825965ca4b8609a239d5a3
Peter Kwamena Essibu <164653045+peterkessibu@users.noreply.github.com>
2024-09-13 17:01:07 +0000

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import {
   ClerkProvider
 } from '@clerk/nextjs'
 import './globals.css'
-import { Poppins } from "next/font/google";
-const poppins = Poppins({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
 
 export const metadata = {
   title: "LearnTab",
@@ -18,7 +16,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className="poppins">
+        <body>
           {children}
         </body>
       </html>
