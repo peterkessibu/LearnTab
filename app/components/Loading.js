4942e36457f085372708adba6dc79ab532951e25
Peter Kwamena Essibu <164653045+peterkessibu@users.noreply.github.com>
2024-09-13 07:44:23 +0000

    Loading UI updated

---
+++ app/components/Loading.js
@@ -0,0 +1,20 @@
+// components/loading.tsx
+import React from "react";
+import { newtonsCradle } from 'ldrs'
+
+newtonsCradle.register()
+
+    
+const Loading = () => {
+    return (
+            <div className="loader">
+                < l-newtons-cradle
+                    size="100"
+                    speed="1.4"
+                    color="blue"
+                ></l-newtons-cradle>
+            </div>
+    );
+};
+
+export default Loading;
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,39 @@
 
 import Link from 'next/link'
 import { useUser } from '@clerk/nextjs'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Loading from './components/loading'
 
 export default function HomePage() {
   const { isSignedIn, user } = useUser()
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 2450)
+    return () => clearTimeout(timer)
+  }, [])
+  if (isLoading) {
+    return (
+      <div className="bg-white flex min-h-screen w-full justify-center items-center">
+          <div className='flex flex-colitems-enter justify-center'>
+            <p className="text-6xl text-blue-900 font-bold mb-4">
+                LearnTab
+            </p>
+            <Loading />
+          </div>
+      </div>
+    )
+  }
 
   return (
     <div className="flex flex-col bg-[#dedeff]">
       <Navbar />
-      <div className="flex-grow container mx-auto px-6 py-12">
-        <h2 className="text-4xl font-bold text-center mb-8">Welcome to LearnTab</h2>
+      <div className="flex-grow container mx-auto p-8">
+        <p className="text-3xl font-bold text-center mb-4">Welcome to LearnTab</p>
         
         {isSignedIn ? (
           <div className="text-center m-4">
@@ -30,34 +52,34 @@ export default function HomePage() {
         )}
 
         {/* 6 Cards explaining the use of flashcards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-[#0b1e36]">
-          <div className="bg-white shadow-md rounded-lg p-6 text-center">
-            <h3 className="text-2xl font-semibold mb-4">Active Learning</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-[#0b1e36]">
+          <div className="bg-white shadow-md rounded-lg p-4 text-center">
+            <p className="text-xl font-semibold mb-2">Active Learning</p>
             <p>Flashcards promote active recall, which is proven to be one of the most effective learning techniques.</p>
           </div>
 
-          <div className="bg-white shadow-md rounded-lg p-6 text-center">
-            <h3 className="text-2xl font-semibold mb-4">Math</h3>
+          <div className="bg-white shadow-lg rounded-lg p-4 text-center">
+            <p className="text-xl font-semibold mb-2">Math</p>
             <p>Master complex formulas and equations with math flashcards. Reinforce important mathematical concepts through repetition.</p>
           </div>
 
-          <div className="bg-white shadow-md rounded-lg p-6 text-center">
-            <h3 className="text-2xl font-semibold mb-4">Science</h3>
+          <div className="bg-white shadow-lg rounded-lg p-4 text-center">
+            <p className="text-xl font-semibold mb-2">Science</p>
             <p>Flashcards help in memorizing important scientific facts, such as biological processes and chemical reactions.</p>
           </div>
 
-          <div className="bg-white shadow-md rounded-lg p-6 text-center">
-            <h3 className="text-2xl font-semibold mb-4">Language Learning</h3>
+          <div className="bg-white shadow-lg rounded-lg p-4 text-center">
+            <p className="text-xl font-semibold mb-2">Language Learning</p>
             <p>Learn new vocabulary, grammar rules, and phrases efficiently by using flashcards for language learning.</p>
           </div>
 
-          <div className="bg-white shadow-md rounded-lg p-6 text-center">
-            <h3 className="text-2xl font-semibold mb-4">History</h3>
+          <div className="bg-white shadow-lg rounded-lg p-4 text-center">
+            <p className="text-xl font-semibold mb-2">History</p>
             <p>Memorize historical dates, events, and figures with flashcards, which makes learning history more engaging and effective.</p>
           </div>
 
-          <div className="bg-white shadow-md rounded-lg p-6 text-center">
-            <h3 className="text-2xl font-semibold mb-4">Exam Preparation</h3>
+          <div className="bg-white shadow-lg rounded-lg p-4 text-center">
+            <p className="text-xl font-semibold mb-2">Exam Preparation</p>
             <p>Prepare for your exams by using flashcards for quick review sessions and focused study breaks.</p>
           </div>
         </div>
